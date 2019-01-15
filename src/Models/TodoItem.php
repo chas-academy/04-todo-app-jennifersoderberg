0f64fccce733374891ef8ea1e47ccfa6dc95cd7a
Jennifer Soederberg <jennifer.soderberg@chasacademy.se>
2019-01-15 15:05:31 +0100

    Add functionality for creating items

--- src/Models/TodoItem.php
+++ src/Models/TodoItem.php
@@ -6,11 +6,16 @@ class TodoItem extends Model
 {
     const TABLENAME = 'todos'; // This is used by the abstract model, don't touch
 
-    // public static function createTodo($title)
-    // {
-    //     // TODO: Implement me!
-    //     // Create a new todo
-    // }
+    public static function createTodo($title)
+    {
+        // TODO: Implement me!
+        // Create a new todo
+        $query = "INSERT INTO todos (title, created) VALUES ('$title', now())";
+        static::$db->query($query);
+        $result = self::$db->execute();
+
+        return $result;
+    }
 
     // // public static function updateTodo($todoId, $title, $completed = null)
     // // {
