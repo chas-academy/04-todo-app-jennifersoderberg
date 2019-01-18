eb66794cf90b1b2cd65ae6fa5f0b22bc8013bf6f
Jennifer Soederberg <jennifer.soderberg@chasacademy.se>
2019-01-18 17:25:43 +0100

    Change placement of the filter buttons. Add CSS styling

--- src/Views/partials/footer.php
+++ src/Views/partials/footer.php
@@ -1,10 +1,16 @@
 <footer class="footer">
     <span class="todo-count"><?= count(array_filter($todos, function($todo) { return $todo['completed'] === "false"; })) ?> item<?= "".count($todos) !== 1 ? "s" : "" ?> left</span>
 
+    <ul class="filters">
+        <li><a href="/ ">All</a></li>
+        <li><a href="/todos/filter-not-completed">Active</a></li>
+        <li><a href="/todos/filter-completed">Completed</a></li>
+    </ul>
+
     <form class="view" method="POST" action="/todos/clear-completed/">
         <button class="clear-completed">Clear completed</button>
     </form>
-
+    
 </footer>
 
 </main>
@@ -13,10 +19,6 @@
     <div class="small-container">
         <p class="text-center">Made by <a href="#">Jennifer Söderberg</a></p>
     </div>
-
-    <a href="/ ">All</a>
-    <a href="/todos/filter-not-completed">Active</a>
-    <a href="/todos/filter-completed">Completed</a>
 </footer>
 
 <script type="module" src="<?= $this->getScript('scripts'); ?>"></script>
